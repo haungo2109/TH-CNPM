3ec47e5e8238785c514f5d86b67df3651fe259f3
kaneki-sama <ngovanhau1999@gmail.com>
2020-11-05 09:07:52 +0700

    xong thuc hanh so 2, listproduct_id, filer by category

--- saleappv1/__init__.py
+++ saleappv1/__init__.py
@@ -1,2 +1,2 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
--- saleappv1/main.py
+++ saleappv1/main.py
@@ -1,17 +1,27 @@
-from flask import render_template
+from flask import render_template, request
 from saleappv1 import app, utils
 
 
 @app.route("/")
 def hello_world():
-    products = utils.read_data()
-    return render_template('index.html', products=products)
+    category = utils.read_data(path="data/category.json")
+    return render_template('index.html', category=category)
 
 
 @app.route('/products')
 def products_list():
-    products = utils.read_data()
+    category = request.args.get("category")
+    if (category):
+        products=utils.get_product_by_cate(cate_id=category)
+    else:
+        products = utils.read_data()
     return render_template('product-list.html', products=products)
+
+
+@app.route("/products/<int:id_product>")
+def product_detail(id_product):
+    product=utils.get_product_by_id(id_product)
+    return render_template('product-detail.html', product=product)
 
 
 if __name__ == "__main__":
